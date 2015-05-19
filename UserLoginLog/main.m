41dcfd6892826c0c8947ce01d6c8f856b9cad43a
Christer Ulfsparre <christer@Christers-MacBook-Pro.local>
2015-05-19 17:39:39 +0200

    Initial Commit

---
+++ UserLoginLog/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UserLoginLog
+//
+//  Created by Christer Ulfsparre on 19/05/15.
+//  Copyright (c) 2015 Christer Ulfsparre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
